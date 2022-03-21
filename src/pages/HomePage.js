e2c29e8a8f56bb856933f5bbbd8d9b7ab10a7d5c
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-03-21 23:47:02 +0200

    new-arrival component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,21 @@ import React, { Component, Fragment } from 'react';
 import FeaturedProducts from '../components/home/FeaturedProducts';
 import Categories from '../components/home/Categories';
 import Collection from '../components/home/Collection';
+import NewArrival from '../components/home/NewArrival';
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        {/* menu & hero section */}
         <FeaturedProducts />
-        <Categories />
+        <NewArrival />
         <Collection />
+        {/* this will be the slider with popular products */}
+        <Categories />
+        {/* banner */}
+        {/* reviews */}
+        {/* footer */}
       </Fragment>
     );
   }
